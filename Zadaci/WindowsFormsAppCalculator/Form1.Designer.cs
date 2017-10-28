8119db102c73be36f7f27033a09b90f36060a273
Domagoj Peršić <domagoj.persic@vsmti.hr>
2017-10-28 20:52:36 +0200

    Zadaca

---
+++ Zadaci/WindowsFormsAppCalculator/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsAppCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inptFirstNumber = new System.Windows.Forms.NumericUpDown();
+            this.inptSecondNumber = new System.Windows.Forms.NumericUpDown();
+            this.inptResult = new System.Windows.Forms.TextBox();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.lblSecondNumber = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inptFirstNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inptSecondNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inptFirstNumber
+            // 
+            this.inptFirstNumber.Location = new System.Drawing.Point(77, 32);
+            this.inptFirstNumber.Name = "inptFirstNumber";
+            this.inptFirstNumber.Size = new System.Drawing.Size(120, 20);
+            this.inptFirstNumber.TabIndex = 0;
+            // 
+            // inptSecondNumber
+            // 
+            this.inptSecondNumber.Location = new System.Drawing.Point(77, 73);
+            this.inptSecondNumber.Name = "inptSecondNumber";
+            this.inptSecondNumber.Size = new System.Drawing.Size(120, 20);
+            this.inptSecondNumber.TabIndex = 1;
+            // 
+            // inptResult
+            // 
+            this.inptResult.Enabled = false;
+            this.inptResult.Location = new System.Drawing.Point(77, 110);
+            this.inptResult.Name = "inptResult";
+            this.inptResult.Size = new System.Drawing.Size(100, 20);
+            this.inptResult.TabIndex = 2;
+            // 
+            // lblFirstNumber
+            // 
+            this.lblFirstNumber.AutoSize = true;
+            this.lblFirstNumber.Location = new System.Drawing.Point(23, 34);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(48, 13);
+            this.lblFirstNumber.TabIndex = 3;
+            this.lblFirstNumber.Text = "Prvi broj:";
+            // 
+            // lblSecondNumber
+            // 
+            this.lblSecondNumber.AutoSize = true;
+            this.lblSecondNumber.Location = new System.Drawing.Point(16, 75);
+            this.lblSecondNumber.Name = "lblSecondNumber";
+            this.lblSecondNumber.Size = new System.Drawing.Size(55, 13);
+            this.lblSecondNumber.TabIndex = 4;
+            this.lblSecondNumber.Text = "Drugi broj:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(25, 113);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(46, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Rezultat";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(252, 29);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "(+)Zbroji";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(252, 65);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubstract.TabIndex = 7;
+            this.btnSubstract.Text = "(-)Oduzmi";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(252, 103);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "(*)Pomnoži";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(252, 141);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "(%)Podijeli";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 213);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblSecondNumber);
+            this.Controls.Add(this.lblFirstNumber);
+            this.Controls.Add(this.inptResult);
+            this.Controls.Add(this.inptSecondNumber);
+            this.Controls.Add(this.inptFirstNumber);
+            this.Name = "Form1";
+            this.Text = "Calculator 1.0v";
+            ((System.ComponentModel.ISupportInitialize)(this.inptFirstNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inptSecondNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown inptFirstNumber;
+        private System.Windows.Forms.NumericUpDown inptSecondNumber;
+        private System.Windows.Forms.TextBox inptResult;
+        private System.Windows.Forms.Label lblFirstNumber;
+        private System.Windows.Forms.Label lblSecondNumber;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+    }
+}
+
